7d82ad24c726308cfa0eb9560b0ea6c919ee9a69
Peerapon <dotaword023@hotmail.com>
2018-05-09 12:25:29 +0700

    StudentApp

---
+++ src/lab7/StudentApp.java
@@ -0,0 +1,18 @@
+package lab7;
+
+
+public class StudentApp {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("MIT");
+        s1.setAge(22);
+        s1.setGender("Male");
+        s1.setHeight(180);
+        System.out.println(s1.toString());
+
+        Student s2 = new Student("Safe",90,"Female", 190);
+        System.out.println(s2.toString());
+
+
+    }
+}
