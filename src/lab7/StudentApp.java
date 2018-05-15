f58608d3ae7b9011e848db387331df67375fe1e1
Peerapon Chaipaen <dotaword023@hotmail.com>
2018-05-15 13:58:11 +0700

    StudentApp Input From user

--- src/lab7/StudentApp.java
+++ src/lab7/StudentApp.java
@@ -1,6 +1,8 @@
 package lab7;
 
 
+import java.util.Scanner;
+
 public class StudentApp {
     public static void main(String[] args) {
         Student s1 = new Student();
@@ -13,6 +15,26 @@ public class StudentApp {
         Student s2 = new Student("Safe",90,"Female", 190);
         System.out.println(s2.toString());
 
+        //input data from user
+        Student s3 = new Student();
+        s3 = inputData(s3);
+        System.out.println(s3.toString());
 
+    }
+
+    private static Student inputData(Student s3) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your information: ");
+        System.out.print("Name: ");
+        s3.setName(sc.nextLine());
+        System.out.print("Age: ");
+        s3.setAge(sc.nextInt());
+        System.out.print("Gender: ");
+        s3.setGender(sc.nextLine());
+        System.out.print("Height: ");
+        s3.setHeight(sc.nextDouble());
+
+
+        return s3;
     }
 }
